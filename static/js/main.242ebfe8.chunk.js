45fd2bc62ec5e606d864ee8c660558c20f711e03
hrishu22 <35893743+hrishu22@users.noreply.github.com>
2021-04-17 22:32:39 +0530

    Updates

---
+++ static/js/main.242ebfe8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcalculator=this.webpackJsonpcalculator||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),a=n(4),r=n.n(a),i=(n(3),n(5)),o=n(0),s=(new Date).toLocaleTimeString(),l=function(){var e=Object(c.useState)(s),t=Object(i.a)(e,2),n=t[0],a=t[1];return setInterval((function(){s=(new Date).toLocaleTimeString(),a(s)}),1e3),Object(o.jsx)(o.Fragment,{children:Object(o.jsx)("div",{className:"time",children:n})})};r.a.render(Object(o.jsx)(l,{}),document.getElementById("root"))},3:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.242ebfe8.chunk.js.map
